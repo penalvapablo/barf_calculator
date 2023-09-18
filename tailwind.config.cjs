a26a7ba22150a03ca05afbc0077923f3a0fd66ad
Pablo Penalva <penalva.pablo@gmail.com>
2023-09-18 14:09:04 -0300

    fonts and colors tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    colors: {
+      one: "#545871",
+      two: "#F7ECEB",
+      three: "#EBD0CE",
+      four: "#FFFFFF",
+      five: "#84889C",
+    },
+    extend: {
+      fontFamily: {
+        title: ["Della Respira", ...defaultTheme.fontFamily.sans],
+        text: ["Raleway Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
